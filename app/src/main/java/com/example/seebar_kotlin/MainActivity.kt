1c91d7e503595ac1769ecf875eba06fd5c50ff91
besrour walid <walidbesrour@gmail.com>
2021-02-11 10:54:34 +0100

    seekBar

--- app/src/main/java/com/example/seebar_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/seebar_kotlin/MainActivity.kt
@@ -2,11 +2,47 @@ package com.example.seebar_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        println("zzzzzzzzzzzzzzzz")
+
+
+
+        val initialTextViewTranslationY = textView_progress.translationY
+
+        seekBar1.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                textView_progress.text = progress.toString()
+
+                val translationDistance = (initialTextViewTranslationY +
+                        progress * resources.getDimension(R.dimen.text_anim_step) * -1)
+
+                textView_progress.animate().translationY(translationDistance)
+                if (!fromUser)
+                    textView_progress.animate().setDuration(500).rotationBy(360f)
+                            .translationY(initialTextViewTranslationY)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+        })
+
+        button_reset.setOnClickListener { v ->
+            seekBar1.progress = 0
+        }
+
     }
 }
